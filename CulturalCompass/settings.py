361b4e103c59e2a130f5ff695400548ebd3cc20e
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-10-10 22:12:32 +0400

    COMMITED

--- CulturalCompass/settings.py
+++ CulturalCompass/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'map',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
