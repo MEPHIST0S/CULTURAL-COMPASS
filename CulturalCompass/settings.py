05d87dd711005d5af22cdee81c4499d4d91cac37
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-10-11 14:36:21 +0400

    COMMITED

--- CulturalCompass/settings.py
+++ CulturalCompass/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'CulturalCompass.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'CCompass',
-        "PASSWORD": '4444',
-        "USER": 'root',
-        "HOST": '127.0.0.1',
-        "PORT": 5432,
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": 'CCompass',
+#         "PASSWORD": '4444',
+#         "USER": 'root',
+#         "HOST": '127.0.0.1',
+#         "PORT": 5432,
+#     }
+# }
 
 
 # Password validation
