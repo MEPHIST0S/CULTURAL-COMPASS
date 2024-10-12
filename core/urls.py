1f926d8c2153f64a8c313b23cce40f86b1be8d54
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-10-12 12:13:53 +0400

    COMMITED

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from core.views import home
+from core.views import home, about
 
 app_name = "core"
 
 urlpatterns = [
     path("", home, name="home"),
-    
+    path("about/", about, name="about")
 ]
